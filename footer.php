791caf83ef6585ce23e6376c99c8313ddc8b8ef9
Morgane33127 <morgane.gutierrez33@gmail.com>
2023-09-29 19:51:14 +0200

    final version

--- footer.php
+++ footer.php
@@ -7,7 +7,8 @@ require 'header.php';
 <footer id="footer">
     <h2 class="text-center pt-3"><a role="button" class="btn btn-success" href="contact.php">Contactez-nous</a></h2>
   </footer>
-    <script src="src/bootstrap.bundle.min"></script>
+    <script src="src/bootstrap.bundle.min.js"></script>
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script>
     const ttx = document.getElementById('chart');
